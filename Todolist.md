1e71995d7f8f8a48496d8dd9085acce654b5a3ff
jacobcheatley <jacobcheatley@gmail.com>
2016-01-02 19:18:29 +1300

    To 0.10.0a0 async version of discord.py

---
+++ Todolist.md
@@ -0,0 +1,8 @@
+#TODO
+!remindme {time} {message} - send private messages in return
+!queue {youtube url} - adds youtube video to music queue to play in Music channel
+!wiki {term} - display link to wikipedia page
+!define {word} - displays definition of word
+!uptime - display time bot has been running
+!roll {number} - rolls an n sided dice
+!flip - flips a coin
--- discordbot.py
+++ discordbot.py
@@ -1,55 +1,33 @@
 import discord
 import os
-import goslate
 
-gs = goslate.Goslate()
-langs = sorted(gs.get_languages().items(), key=lambda t: t[1])
+client = discord.Client()
 
 # Constants:
 valid_commands = [
     '!help - Displays this help.',
-    '!echo {message} - Echoes the message back.',
-    '!translate {to} - Translates the message in whatever language to the chosen language.',
-    '!languages - See a list of languages '
+    '!echo {message} - Echoes the message back.'
 ]
 
 help_text = ', it seems you need help.\nFor now, these are the chat commands:\n```\n{}```'.format('\n'.join(valid_commands))
 
-client = discord.Client()
-client.login(os.environ['DISCORD_BOT_USER'], os.environ['DISCORD_BOT_PASS'])
-
 
 @client.event
-def on_ready():
-    print('Connected!')
-    print(client.user.name, client.user.id)
-
-
-@client.event
-def on_message(message):
-    if message.author == client.user:
+async def on_message(message):
+    if message.author.id == client.user.id:
         return
 
     if message.content.startswith('!echo'):
-        client.send_message(message.channel, message.content[6:])
+        await client.send_message(message.channel, message.content[6:])
 
     if message.content.startswith('!help'):
-        client.send_message(message.channel, message.author.mention() + help_text)
-
-    if message.content.startswith('!translate'):
-        parts = message.content.split(' ')
-        lang = parts[1]
-        content = ' '.join(parts[2:])
-        client.send_message(message.channel, gs.translate(content, lang))
-
-    if message.content.startswith('!languages'):
-        client.send_message(message.channel, ', '.join(['{}: {}'.format(k, v) for (k, v) in langs]))
+        await client.send_message(message.channel, message.author.mention + help_text)
 
 
 @client.event
-def on_member_join(member):
+async def on_member_join(member):
     server = member.server
-    client.send_message(server, 'Welcome {0} to {1.name}!'.format(member.mention(), server))
+    client.send_message(server, 'Welcome {0} to {1.name}!'.format(member.mention, server))
 
 
-client.run()
+client.run(os.environ['DISCORD_BOT_USER'], os.environ['DISCORD_BOT_PASS'])
