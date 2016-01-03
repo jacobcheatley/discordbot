7c1f4eb68fdf90f1bad5478a2f80de25dccd77ce
jacobcheatley <jacobcheatley@gmail.com>
2016-01-04 10:27:39 +1300

    Commands system.

---
+++ botcommands.py
@@ -0,0 +1,166 @@
+import random
+import os
+import youtube_dl
+import config
+from collections import OrderedDict
+
+# Constants:
+ydl_options = {'buffer_size': 4096,
+               'socket_timeout': 3,
+               'retries': 20,
+               'format': 'bestaudio/best',
+               'postprocessors': [{
+                   'key': 'FFmpegExtractAudio',
+                   'preferredcodec': 'mp3'
+               }],
+               'outtmpl': '%(id)s.%(ext)s',
+               'restrict_filenames': True,
+               'get_filename': True
+               }
+yt_downloader = youtube_dl.YoutubeDL(ydl_options)
+next_song_format = 'Now playing {0.title} from {0.requester.mention}'
+
+
+class CommandInfo:
+    def __init__(self, func, args, docs):
+        self.func = func
+        self.args = args
+        self.docs = docs
+
+    def __str__(self):
+        return '{0}- {1}'.format('' if self.args == '' else self.args + ' ', self.docs)
+
+
+def help_text(author):
+    commands_display = '\n'.join(
+        ['{0}{1} {2}'.format(config.prefix, key, str(value)) for (key, value) in commands.items()])
+    return '{0}, it seems you need help.\nFor now, these are the chat commands:\n```\n{1}```'.format(author.mention,
+                                                                                                     commands_display)
+
+
+async def help_disp(bot=None, message=None, admin=False, args=None):
+    await bot.send_message(message.channel, help_text(message.author))
+
+
+async def whomai(bot=None, message=None, admin=False, args=None):
+    await bot.send_message(message.channel, message.author.id)
+
+
+async def echo(bot=None, message=None, admin=False, args=None):
+    try:
+        await bot.send_message(message.channel, ' '.join(args[1:]))
+    except IndexError:
+        pass
+
+
+async def roll(bot=None, message=None, admin=False, args=None):
+    try:
+        n = int(args[1])
+    except:  # ayy lmao general exception
+        n = 6
+    if n > 0:
+        roll = random.randint(1, n)
+        await bot.send_message(message.channel, 'Rolled {0}'.format(roll))
+
+
+async def flip(bot=None, message=None, admin=False, args=None):
+    await bot.send_message(message.channel, random.choice(['Heads', 'Tails']))
+
+
+async def eightball(bot=None, message=None, admin=False, args=None):
+    await bot.send_message(message.channel, random.choice([
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes, definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Most likely',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+        'Reply hazy try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+        'Don\'t count on it',
+        'My reply is no',
+        'My sources say no',
+        'Outlook not so good',
+        'Very doubtful'
+    ]))
+
+# region Audio
+class SongEntry:
+    def __init__(self, filename, title, requester):
+        self.filename = filename
+        self.title = title
+        self.requester = requester
+
+
+async def setup(bot=None, message=None, admin=False, args=None):
+    if admin and not bot.setup_done:
+        bot.setup_done = True
+        bot.jukebox_text_channel = message.channel
+        await bot.join_voice_channel(message.author.voice_channel)
+        await song_loop(bot)
+
+
+async def queue(bot=None, message=None, admin=False, args=None):
+    if not bot.setup_done:
+        return
+
+    try:
+        url = args[1].split('&')[0]
+        with yt_downloader:
+            try:
+                await bot.send_message(message.channel, 'Adding song to queue.')
+                info = yt_downloader.extract_info(url, download=False)
+                filename = '{0}.mp3'.format(info.get('id', None))
+                title = info.get('title', None)
+                if not os.path.isfile(filename):
+                    yt_downloader.download([url])
+                await bot.song_queue.put(SongEntry(filename, title, message.author))
+            except youtube_dl.DownloadError:
+                await bot.send_message(message.channel, 'Failed to download song.')
+    except IndexError:
+        pass
+
+
+async def song_loop(bot):
+    while True:
+        bot.play_next_song.clear()
+        bot.current = await bot.song_queue.get()
+        try:
+            bot.player = bot.voice.create_ffmpeg_player(bot.current.filename, after=goto_next_song(bot))
+            await bot.send_message(bot.jukebox_text_channel, next_song_format.format(bot.current))
+            bot.player.start()
+        except:  # TODO: MAKE THIS NOT STUPID
+            goto_next_song(bot)
+            await bot.send_message(bot.jukebox_text_channel, 'Woops I can\'t play this song lmao.')
+        await bot.play_next_song.wait()
+
+
+def goto_next_song(bot):
+    bot.loop.call_soon_threadsafe(bot.play_next_song.set)
+
+# endregion
+
+commands = OrderedDict([
+    ('help', CommandInfo(help_disp, '', 'Displays this help text.')),
+    ('whoami', CommandInfo(whomai, '', 'Gives your user ID.')),
+    ('echo', CommandInfo(echo, '{message}', 'Echoes the message back.')),
+    ('roll', CommandInfo(roll, '{number}', 'Rolls an n sided die (default 6).')),
+    ('flip', CommandInfo(flip, '', 'Flips a coin.')),
+    ('8ball', CommandInfo(eightball, '{query}', 'The Magic 8 Ball has the answers to all questions.')),
+    # ('queue', CommandInfo(queue, '{youtube url}', 'Adds a YouTube song to the Jukebox queue.')),
+    # ('setup', CommandInfo(setup, '', 'Sets up the Jukebox, admin only.'))
+])
+
+
+async def command(bot, message, admin):
+    args = message.content.replace(config.prefix, '', 1).split(' ')
+    command_info = commands.get(args[0], None)
+    if command_info is not None:
+        await command_info.func(bot=bot, message=message, admin=admin, args=args)
--- discordbot.py
+++ discordbot.py
@@ -1,60 +1,15 @@
 import discord
 import os
-import random
 import asyncio
-import youtube_dl
-
+import botcommands
+import config
 
 # Set up downloader and opus
-def ydl_hook(d):
-    if d['status'] == 'finished':
-        print('Downloaded')
-
 if not discord.opus.is_loaded():
     if os.name != 'nt':
         discord.opus.load_opus('/app/lib/opus/lib/libopus.so.0.5.1')
     else:
         discord.opus.load_opus('opus.dll')
-
-
-ydl_options = {'buffer_size': 4096,
-               'socket_timeout': 3,
-               'retries': 20,
-               'format': 'bestaudio/best',
-               'postprocessors': [{
-                   'key': 'FFmpegExtractAudio',
-                   'preferredcodec': 'mp3'
-               }],
-               'progress_hooks': [ydl_hook],
-               'outtmpl': '%(id)s.%(ext)s',
-               'restrict_filenames': True,
-               'get_filename': True
-               }
-
-yt_downloader = youtube_dl.YoutubeDL(ydl_options)
-
-# Constants:
-valid_commands = [
-    '!help - Displays this help.',
-    '!echo {message} - Echoes the message back.',
-    '!roll {number} - Rolls an n sided dice (default 6).',
-    '!flip - Flips a coin.',
-    '!queue {youtube url} - Adds a youtube song to the jukebox queue.'
-]
-
-help_text = 'it seems you need help.\nFor now, these are the chat commands:\n```\n{}```'.format(
-    '\n'.join(valid_commands))
-coin_flips = ['Heads', 'Tails']
-
-next_song_format = 'Now playing {0.title} from {0.requester.mention}'
-
-
-# Classes:
-class SongEntry:
-    def __init__(self, filename, title, requester):
-        self.filename = filename
-        self.title = title
-        self.requester = requester
 
 
 class DankBot(discord.Client):
@@ -65,68 +20,15 @@ class DankBot(discord.Client):
         self.current = None
         self.player = None
         self.jukebox_text_channel = None
-        self.setup = False
-
-    async def song_loop(self):
-        while True:
-            self.play_next_song.clear()
-            self.current = await self.song_queue.get()
-            try:
-                self.player = self.voice.create_ffmpeg_player(self.current.filename, after=self.goto_next_song)
-                await self.send_message(self.jukebox_text_channel, next_song_format.format(self.current))
-                self.player.start()
-            except:  # TODO: MAKE THIS NOT STUPID
-                self.goto_next_song()
-                await self.send_message(self.jukebox_text_channel, 'Woops I can\'t play this song lmao.')
-            await self.play_next_song.wait()
-
-    def goto_next_song(self):
-        self.loop.call_soon_threadsafe(self.play_next_song.set)
+        self.setup_done = False
 
     async def on_message(self, message):
-        if message.author.id == self.user.id:
+        if message.author.id == self.user.id or not message.content.startswith(config.prefix):
             return
 
-        if message.content.startswith('!echo'):
-            await self.send_message(message.channel, message.content[5:].strip())
+        admin = message.author.id in config.admins
 
-        if message.content.startswith('!help'):
-            await self.send_message(message.channel, '{0}, {1}'.format(message.author.mention, help_text))
-
-        if message.content.startswith('!roll'):
-            try:
-                n = int(message.content[5:].strip())
-            except:  # ayy lmao general exception
-                n = 6
-            if n > 0:
-                roll = random.randint(1, n)
-                await self.send_message(message.channel, 'Rolled {0}'.format(roll))
-
-        if message.content.startswith('!flip'):
-            await self.send_message(message.channel, random.choice(coin_flips))
-
-        if message.content.startswith('!setup') and not self.setup:
-            self.setup = True
-            self.jukebox_text_channel = message.channel
-            await self.join_voice_channel(message.author.voice_channel)
-            await self.song_loop()
-
-        if message.content.startswith('!queue') and self.setup and message.channel == self.jukebox_text_channel:
-            url = message.content[6:].strip()
-            with yt_downloader:
-                try:
-                    await self.send_message(message.channel, 'Adding song to queue.')
-                    info = yt_downloader.extract_info(url, download=False)
-                    filename = '{0}.mp3'.format(info.get('id', None))
-                    title = info.get('title', None)
-                    if not os.path.isfile(filename):
-                        yt_downloader.download([url])
-                    await self.song_queue.put(SongEntry(filename, title, message.author))
-                except youtube_dl.DownloadError:
-                    await self.send_message(message.channel, 'Failed to download song.')
-                # except:
-                #     raise
-                #     await self.send_message(message.channel, 'Invalid URL.')
+        await botcommands.command(self, message, admin)
 
     async def on_member_join(self, member):
         server = member.server
