e30de34f05b735cc16d53651484213744d5b8bb7
jacobcheatley <jacobcheatley@gmail.com>
2016-01-03 13:30:46 +1300

    Please freaking work Linux music.

--- discordbot.py
+++ discordbot.py
@@ -2,12 +2,15 @@ import discord
 import os
 import random
 import asyncio
+import youtube_dl
 
 if not discord.opus.is_loaded():
     if os.name != 'nt':
         discord.opus.load_opus('/app/lib/opus/lib/libopus.so.0.5.1')
+        ytdl_options = '--ffmpeg-location /app -x --buffer-size 4096 --socket-timeout 3 --retries 20 --prefer-insecure'
     else:
         discord.opus.load_opus('opus.dll')
+        ytdl_options = '-x --buffer-size 4096 --socket-timeout 3 --retries 20 --prefer-insecure'
 
 # Constants:
 valid_commands = [
@@ -18,7 +21,8 @@ valid_commands = [
     '!queue {youtube url} - Adds a youtube song to the jukebox queue.'
 ]
 
-help_text = 'it seems you need help.\nFor now, these are the chat commands:\n```\n{}```'.format('\n'.join(valid_commands))
+help_text = 'it seems you need help.\nFor now, these are the chat commands:\n```\n{}```'.format(
+    '\n'.join(valid_commands))
 coin_flips = ['Heads', 'Tails']
 
 next_song_format = 'Now playing {0.url} from {0.requester.mention}'
@@ -28,7 +32,6 @@ class SongEntry:
     def __init__(self, url, message):
         self.url = url
         self.requester = message.author
-        self.channel = message.channel
 
 
 class DankBot(discord.Client):
@@ -38,15 +41,20 @@ class DankBot(discord.Client):
         self.play_next_song = asyncio.Event()
         self.current = None
         self.player = None
+        self.jukebox_text_channel = None
         self.setup = False
 
     async def song_loop(self):
         while True:
             self.play_next_song.clear()
             self.current = await self.song_queue.get()
-            self.player = self.voice.create_ytdl_player(self.current.url, after=self.goto_next_song, options='--ffmpeg-location /app')
-            self.player.start()
-            await self.send_message(self.current.channel, next_song_format.format(self.current))
+            try:
+                self.player = self.voice.create_ytdl_player(self.current.url, after=self.goto_next_song, options=ytdl_options)
+                self.player.start()
+                await self.send_message(self.jukebox_text_channel, next_song_format.format(self.current))
+            except youtube_dl.DownloadError:
+                self.goto_next_song()
+                await self.send_message(self.jukebox_text_channel, 'Woops I can\'t play this song lmao.')
             await self.play_next_song.wait()
 
     def goto_next_song(self):
@@ -74,15 +82,16 @@ class DankBot(discord.Client):
         if message.content.startswith('!flip'):
             await self.send_message(message.channel, random.choice(coin_flips))
 
-        if message.content.startswith('!queue') and self.setup:
+        if message.content.startswith('!setup') and not self.setup:
+            self.setup = True
+            self.jukebox_text_channel = message.channel
+            await self.join_voice_channel(message.author.voice_channel)
+            await self.song_loop()
+
+        if message.content.startswith('!queue') and self.setup and message.channel == self.jukebox_text_channel:
             url = message.content[6:].strip()
             await self.song_queue.put(SongEntry(url, message))
             await self.send_message(message.channel, 'Adding song to queue.')
-
-        if message.content.startswith('!setup') and not self.setup:
-            self.setup = True
-            await self.join_voice_channel(message.author.voice_channel)
-            await self.song_loop()
 
     async def on_member_join(self, member):
         server = member.server
